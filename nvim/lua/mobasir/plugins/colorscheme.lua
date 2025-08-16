4bdad186cd02458e1c0b96ee6c89100b4c68e745
MobasirSarkar <mobasirgithubonly@gmail.com>
2025-08-16 10:09:19 +0530

    added the font, changed the tmux.conf , kitty.conf and nvim

--- nvim/lua/mobasir/plugins/colorscheme.lua
+++ nvim/lua/mobasir/plugins/colorscheme.lua
@@ -1,259 +1,257 @@
 return {
-	{
-		"glepnir/oceanic-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local ui = vim.g
-			ui.oceanic_material_allow_italic = 1
-			ui.oceanic_material_allow_underline = 1
-			ui.oceanic_material_transparent_background = 1
-			ui.oceanic_material_background = "ocean"
-			ui.oceanic_material_allow_undercurl = 1
-			ui.oceanic_material_allow_bold = 1
-		end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-	},
-	{
-		"sainnhe/gruvbox-material",
-		config = function()
-			vim.g.gruvbox_material_background = "hard"
-			vim.g.gruvbox_material_transparent_background = 2
-		end,
-	},
-	{
-		"Tsuzat/NeoSolarized.nvim",
-		lazy = false,
-		priority = 1000,
-		options = {
-			style = "dark",
-			transparent = true,
-			enable_italics = true,
-			styles = {
-				comments = { italic = true },
-				keywords = { bold = true },
-				functions = { italic = true },
-				variables = {},
-				string = { italic = true },
-				underline = true,
-				undercurl = true,
-			},
-		},
-		config = function()
-			require("NeoSolarized").setup()
-		end,
-	},
-	{
-		"neanias/everforest-nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("everforest").setup({
-				background = "hard",
-				transparent_background_level = 1,
-				italics = true,
-				ui_contrast = "high",
-				diagnostic_virtual_text = "colored",
-			})
-		end,
-	},
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			style = "dark",
-			transparent = true,
-			enable_italics = true,
-			styles = {
-				comments = { italic = true },
-				keywords = { bold = true },
-				functions = { italic = true },
-				variables = {},
-				string = { italic = true },
-				underline = true,
-				undercurl = true,
-			},
-		},
-		config = function()
-			require("solarized-osaka").setup()
-		end,
-	},
-	{
-		"HoNamDuong/hybrid.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			require("hybrid").setup({
-				terminal_colors = true,
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = false,
-					emphasis = true,
-					comments = true,
-					folds = true,
-				},
-				strikethrough = true,
-				inverse = true,
-				transparent = true,
-			})
-		end,
-	},
-	{
-		"sho-87/kanagawa-paper.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			require("kanagawa-paper").setup({
-				undercurl = true,
-				transparent = true,
-				commentStyle = { italic = true },
-				functionStyle = { italic = true },
-				color_offset = {
-					canvas = { brightness = 1, saturation = 1 },
-				},
-			})
-		end,
-	},
-	{
-		"AlexvZyl/nordic.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("nordic").setup({
-				bold_keywords = false,
-				italic_comments = true,
-				transparent = {
-					bg = false,
-					float = false,
-				},
-			})
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("rose-pine").setup({
-				variant = "moon",
-				dark_variant = "moon",
-				styles = {
-					transparency = true,
-					italic = true,
-				},
-				highlight_groups = {
-					NormalFloat = { bg = "base", blend = 0 }, -- Set background for floating windows
-					Pmenu = { bg = "base" },
-					PmenuSel = { bg = "overlay", fg = "love" },
-					PmenuBorder = { bg = "base", fg = "highlight_high" },
-					TelescopeBorder = { fg = "highlight_high", bg = "base" },
-					TelescopeNormal = { bg = "base" },
-					TelescopePromptNormal = { bg = "base" },
-					TelescopeResultsNormal = { fg = "subtle", bg = "base" },
-					TelescopeSelection = { fg = "text", bg = "base" },
-				},
-			})
-		end,
-	},
-	{
-		"talha-akram/noctis.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function() end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			style = "storm",
-			transparent = true,
-			styles = { sidebars = "transparent" },
-		},
+  {
+    "glepnir/oceanic-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local ui = vim.g
+      ui.oceanic_material_allow_italic = 1
+      ui.oceanic_material_allow_underline = 1
+      ui.oceanic_material_transparent_background = 1
+      ui.oceanic_material_background = "ocean"
+      ui.oceanic_material_allow_undercurl = 1
+      ui.oceanic_material_allow_bold = 1
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+  {
+    "sainnhe/gruvbox-material",
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_transparent_background = 2
+    end,
+  },
+  {
+    "Tsuzat/NeoSolarized.nvim",
+    lazy = false,
+    priority = 1000,
+    options = {
+      style = "dark",
+      transparent = true,
+      enable_italics = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { bold = true },
+        functions = { italic = true },
+        variables = {},
+        string = { italic = true },
+        underline = true,
+        undercurl = true,
+      },
+    },
+    config = function()
+      require("NeoSolarized").setup()
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        transparent_background_level = 1,
+        italics = true,
+        ui_contrast = "high",
+        diagnostic_virtual_text = "colored",
+      })
+    end,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "dark",
+      transparent = true,
+      enable_italics = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { bold = true },
+        functions = { italic = true },
+        variables = {},
+        string = { italic = true },
+        underline = true,
+        undercurl = true,
+      },
+    },
+    config = function()
+      require("solarized-osaka").setup()
+    end,
+  },
+  {
+    "HoNamDuong/hybrid.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("hybrid").setup({
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = true,
+          comments = true,
+          folds = true,
+        },
+        strikethrough = true,
+        inverse = true,
+        transparent = true,
+      })
+    end,
+  },
+  {
+    "sho-87/kanagawa-paper.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("kanagawa-paper").setup({
+        undercurl = true,
+        transparent = true,
+        commentStyle = { italic = true },
+        functionStyle = { italic = true },
+        color_offset = {
+          canvas = { brightness = 1, saturation = 1 },
+        },
+      })
+    end,
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").setup({
+        bold_keywords = false,
+        italic_comments = true,
+        transparent = {
+          bg = false,
+          float = false,
+        },
+      })
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        dark_variant = "moon",
+        styles = {
+          transparency = true,
+          italic = true,
+        },
+        highlight_groups = {
+          NormalFloat = { bg = "base", blend = 0 }, -- Set background for floating windows
+          Pmenu = { bg = "base" },
+          PmenuSel = { bg = "overlay", fg = "love" },
+          PmenuBorder = { bg = "base", fg = "highlight_high" },
+          TelescopeBorder = { fg = "highlight_high", bg = "base" },
+          TelescopeNormal = { bg = "base" },
+          TelescopePromptNormal = { bg = "base" },
+          TelescopeResultsNormal = { fg = "subtle", bg = "base" },
+          TelescopeSelection = { fg = "text", bg = "base" },
+        },
+      })
+    end,
+  },
+  {
+    "talha-akram/noctis.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function() end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "storm",
+      transparent = true,
+      styles = { sidebars = "transparent" },
+    },
 
-		config = function(_, opts)
-			require("tokyonight").setup(opts)
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			-- Default options:
-			require("kanagawa").setup({
-				compile = true, -- enable compiling the colorscheme
-				undercurl = true, -- enable undercurls
-				commentStyle = { italic = true },
-				functionStyle = {},
-				keywordStyle = { italic = true },
-				statementStyle = { bold = true },
-				typeStyle = {},
-				transparent = true, -- do not set background color
-				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-				terminalColors = true, -- define vim.g.terminal_color_{0,17}
-				theme = "dragon", -- Load "wave" theme
-				background = { -- map the value of 'background' option to a theme
-					dark = "dragon", -- try "dragon" !
-					light = "lotus",
-				},
-			})
-		end,
-	},
-	-- Using lazy.nvim
-	{
-		"metalelf0/black-metal-theme-neovim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("black-metal").setup({
-				theme = "dark-funeral", -- sets variant
-			})
-			vim.cmd([[colorscheme dark-funeral]])
-		end,
-	},
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      -- Default options:
+      require("kanagawa").setup({
+        compile = true, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = true, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        theme = "dragon",  -- Load "wave" theme
+        background = {     -- map the value of 'background' option to a theme
+          dark = "dragon", -- try "dragon" !
+          light = "lotus",
+        },
+      })
+    end,
+  },
+  -- Using lazy.nvim
+  {
+    "metalelf0/black-metal-theme-neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("black-metal").setup({})
+      vim.cmd([[colorscheme burzum]])
+    end,
+  },
 
-	{
-		"vague2k/vague.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other plugins
-		config = function()
-			require("vague").setup({
-				transparent = true,
-				on_highlights = function(highlights, colors) end,
-				colors = {
-					bg = "#171418", -- #14 → +7% red & blue
-					inactiveBg = "#1d1a23",
-					fg = "#c9c0cc",
-					floatBorder = "#756e75",
-					line = "#252228",
-					comment = "#787078",
-					builtin = "#afafb9",
-					func = "#b89aa4",
-					string = "#d1b08f", -- adjusted from duller yellow
-					number = "#c8a782",
-					property = "#c0b8cf",
-					constant = "#b0a8c2",
-					parameter = "#b29ab4",
-					visual = "#2e2f33",
-					error = "#ca7a8f",
-					warning = "#cfa774",
-					hint = "#9296c2",
-					operator = "#929ab2",
-					keyword = "#9099ae",
-					type = "#a1a4b2",
-					search = "#3d465d",
-					plus = "#80936d",
-					delta = "#cfa774",
-				},
-			})
-		end,
-	},
+  {
+    "vague2k/vague.nvim",
+    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      require("vague").setup({
+        transparent = true,
+        on_highlights = function(highlights, colors) end,
+        colors = {
+          bg = "#171418", -- #14 → +7% red & blue
+          inactiveBg = "#1d1a23",
+          fg = "#c9c0cc",
+          floatBorder = "#756e75",
+          line = "#252228",
+          comment = "#787078",
+          builtin = "#afafb9",
+          func = "#b89aa4",
+          string = "#d1b08f", -- adjusted from duller yellow
+          number = "#c8a782",
+          property = "#c0b8cf",
+          constant = "#b0a8c2",
+          parameter = "#b29ab4",
+          visual = "#2e2f33",
+          error = "#ca7a8f",
+          warning = "#cfa774",
+          hint = "#9296c2",
+          operator = "#929ab2",
+          keyword = "#9099ae",
+          type = "#a1a4b2",
+          search = "#3d465d",
+          plus = "#80936d",
+          delta = "#cfa774",
+        },
+      })
+    end,
+  },
 }
--- nvim/lua/mobasir/plugins/lsp.lua
+++ nvim/lua/mobasir/plugins/lsp.lua
@@ -1,148 +1,151 @@
 return {
-	"junnplus/lsp-setup.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"williamboman/mason.nvim", -- optional
-		"williamboman/mason-lspconfig.nvim", -- optional
-		"Saghen/blink.cmp",
-		{ "j-hui/fidget.nvim", opts = {} },
-	},
+  "junnplus/lsp-setup.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",         -- optional
+    "williamboman/mason-lspconfig.nvim", -- optional
+    "Saghen/blink.cmp",
+    { "j-hui/fidget.nvim", opts = {} },
+  },
 
-	config = function()
-		require("lsp-setup").setup({
-			default_mappings = true,
-			on_attach = function(client, bufnr)
-				require("lsp-setup.utils").format_on_save(client)
-			end,
-			inlay_hints = {
-				enabled = true,
-				highlight = "Comment",
-			},
-			--capabilities = vim.lsp.protocol.make_client_capabilities(),
-			capabilities = require("blink.cmp").get_lsp_capabilities(),
-			servers = {
-				erlangls = {},
-				elixirls = {},
-				pyright = {},
-				unocss = {},
-				ts_ls = {},
-				docker_compose_language_service = {},
-				dockerls = {},
-				tailwindcss = {},
-				gopls = {
-					settings = {
-						gopls = {
-							hints = {},
-						},
-					},
-				},
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagonistic = {
-								globals = { "vim" },
-								unusedLocalVariable = true,
-								unusedFunction = true,
-							},
-							hint = {
-								enable = false,
-								arrayIndex = "Auto",
-								await = true,
-								paramName = "All",
-								paramType = true,
-								semicolon = "SameLine",
-								setType = false,
-							},
-						},
-					},
-				},
-				rust_analyzer = {
-					settings = {
-						["rust-analyzer"] = {
-							cargo = {
-								loadOutDirsFromCheck = true,
-							},
-							inlayHints = {
-								bindingModeHints = {
-									enable = false,
-								},
-								chainingHints = {
-									enable = true,
-								},
-								closingBraceHints = {
-									enable = true,
-									minLines = 25,
-								},
-								closureReturnTypeHints = {
-									enable = "never",
-								},
-								lifetimeElisionHints = {
-									enable = "never",
-									useParameterNames = false,
-								},
-								maxLength = 25,
-								parameterHints = {
-									enable = true,
-								},
-								reborrowHints = {
-									enable = "never",
-								},
-								renderColons = true,
-								typeHints = {
-									enable = false,
-									hideClosureInitialization = false,
-									hideNamedConstructor = false,
-								},
-							},
-						},
-					},
-				},
-				zls = {
-					settings = {
-						zls = {
-							enable_inlay_hints = true,
-							inlay_hints_show_builtin = true,
-							inlay_hints_exclude_single_argument = true,
-							inlay_hints_hide_redundant_param_names = false,
-							inlay_hints_hide_redundant_param_names_last_token = false,
-						},
-					},
-				},
-				clangd = {},
-			},
-		})
+  config = function()
+    require("lsp-setup").setup({
+      default_mappings = true,
+      on_attach = function(client, bufnr)
+        require("lsp-setup.utils").format_on_save(client)
+      end,
+      inlay_hints = {
+        enabled = true,
+        highlight = "Comment",
+      },
+      --capabilities = vim.lsp.protocol.make_client_capabilities(),
+      capabilities = require("blink.cmp").get_lsp_capabilities(),
+      servers = {
+        erlangls = {},
+        elixirls = {},
+        pyright = {},
+        unocss = {},
+        ts_ls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        tailwindcss = {},
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {},
+            },
+          },
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagonistic = {
+                globals = { "vim" },
+                unusedLocalVariable = true,
+                unusedFunction = true,
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              hint = {
+                enable = false,
+                arrayIndex = "Auto",
+                await = true,
+                paramName = "All",
+                paramType = true,
+                semicolon = "SameLine",
+                setType = false,
+              },
+            },
+          },
+        },
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                loadOutDirsFromCheck = true,
+              },
+              inlayHints = {
+                bindingModeHints = {
+                  enable = false,
+                },
+                chainingHints = {
+                  enable = true,
+                },
+                closingBraceHints = {
+                  enable = true,
+                  minLines = 25,
+                },
+                closureReturnTypeHints = {
+                  enable = "never",
+                },
+                lifetimeElisionHints = {
+                  enable = "never",
+                  useParameterNames = false,
+                },
+                maxLength = 25,
+                parameterHints = {
+                  enable = false,
+                },
+                reborrowHints = {
+                  enable = "never",
+                },
+                renderColons = true,
+                typeHints = {
+                  enable = false,
+                  hideClosureInitialization = false,
+                  hideNamedConstructor = false,
+                },
+              },
+            },
+          },
+        },
+        zls = {
+          settings = {
+            zls = {
+              enable_inlay_hints = true,
+              inlay_hints_show_builtin = true,
+              inlay_hints_exclude_single_argument = true,
+              inlay_hints_hide_redundant_param_names = false,
+              inlay_hints_hide_redundant_param_names_last_token = false,
+            },
+          },
+        },
+        clangd = {},
+      },
+    })
 
-		vim.diagnostic.config({
-			severity_sort = true,
-			float = { border = "rounded", source = "if_many" },
-			underline = true,
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = "󰅚 ",
-					[vim.diagnostic.severity.WARN] = "󰀪 ",
-					[vim.diagnostic.severity.INFO] = "󰋽 ",
-					[vim.diagnostic.severity.HINT] = "󰌶 ",
-				},
-			},
-			virtual_text = {
-				source = "if_many",
-				spacing = 2,
-				format = function(diagnostic)
-					local diagnostic_message = {
-						[vim.diagnostic.severity.ERROR] = diagnostic.message,
-						[vim.diagnostic.severity.WARN] = diagnostic.message,
-						[vim.diagnostic.severity.INFO] = diagnostic.message,
-						[vim.diagnostic.severity.HINT] = diagnostic.message,
-					}
-					return diagnostic_message[diagnostic.severity]
-				end,
-			},
-		})
+    vim.diagnostic.config({
+      severity_sort = true,
+      float = { border = "rounded", source = "if_many" },
+      underline = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅚 ",
+          [vim.diagnostic.severity.WARN] = "󰀪 ",
+          [vim.diagnostic.severity.INFO] = "󰋽 ",
+          [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+      },
+      virtual_text = {
+        source = "if_many",
+        spacing = 2,
+        format = function(diagnostic)
+          local diagnostic_message = {
+            [vim.diagnostic.severity.ERROR] = diagnostic.message,
+            [vim.diagnostic.severity.WARN] = diagnostic.message,
+            [vim.diagnostic.severity.INFO] = diagnostic.message,
+            [vim.diagnostic.severity.HINT] = diagnostic.message,
+          }
+          return diagnostic_message[diagnostic.severity]
+        end,
+      },
+    })
 
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-			border = "rounded",
-			max_width = 80,
-			focusable = true,
-		})
-	end,
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+      max_width = 80,
+      focusable = true,
+    })
+  end,
 }
